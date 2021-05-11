3157ee3e862f67ad2e5130074bbd218a1d7bb439
8a9phrywet978ui <57111147+8a9phrywet978ui@users.noreply.github.com>
2021-05-11 16:02:54 -0500

    Delete sharepoint-sites-remove-permissions.ps1

--- sharepoint-sites-remove-permissions.ps1
+++
@@ -1,3 +0,0 @@
-﻿Import-Csv C:\scripts\CERT-Migration\sharepoint-sites.csv | foreach-object {
-    $Sharepoint = Set-SPOUser -Site $_.sharepoint -LoginName b81670@andersencorp.com -IsSiteCollectionAdmin $false
-}
